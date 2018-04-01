62765c8a1f9c3278d7fc5ae5e7ee4f9f34c4950e
John Doe Bites Back <33932368+ddbit@users.noreply.github.com>
2018-04-01 19:36:43 +0200

    Update BTree.sol

--- BTree.sol
+++ BTree.sol
@@ -20,7 +20,13 @@ contract BTree{
         nodes.push(nil());
     }
 
-
+    function len() public view returns(uint){
+        uint size=0;
+        for (uint i=0;i<nodes.length;i++){
+            if(!nodes[i].nil) size++;
+        }
+        return size;
+    }
 
     function _push(uint x, uint nodeIdx) internal{
         Node storage tree = nodes[nodeIdx];
@@ -59,6 +65,47 @@ contract BTree{
         return nodes[minIndex(0)].x;
     }
     
+    
+    function remove(uint nodeIdx){
+        Node node = nodes[nodeIdx];
+        
+        Node lesser = nodes[node.lesser];
+        Node greater=nodes[node.greater];
+        Node parent = nodes[node.parent];
+        
+        //case node has no children
+        if(lesser.nil && greater.nil){
+            //has no children
+            //make node a nil node
+            node.nil = true;
+            node.x=0;
+            node.lesser=0;
+            node.greater=0;
+            //keep parent
+            //node.parent unchanged
+        }
+        
+        //case node has exactly one child
+        //and bypass reference to parent
+        if(lesser.nil && !greater.nil){
+            greater.parent = node.parent;
+        }
+        if(!lesser.nil && greater.nil){
+            lesser.parent = node.parent;
+        }
+        
+        //case node has both greater and lesser
+        if(!lesser.nil && !greater.nil){
+            //take the right min
+            Node storage rightMin=nodes[minIndex(node.greater)];
+            //replace this node content with rightMin
+            node.x=rightMin.x;
+            //remove the rightMin from tree -- recursive call
+            remove(minIndex(node.greater));
+   
+        }
+        
+    }
     
     
     
